bcb14e51a6d8ee3938c05ce7add7d2064fced4e5
Aatish Adajania <aatishadajania20@gmail.com>
2026-05-11 22:52:40 +0530

    redesigned for mobile

--- src/data/projects.js
+++ src/data/projects.js
@@ -70,6 +70,78 @@ const projects = [
     image:
       "https://res.cloudinary.com/dta1dl0pj/image/upload/q_auto/f_auto/v1778517727/Afroboss_event_in_london_2024__degan9.jpg",
   },
+
+  {
+    title: "Hype Hijacking Tryst",
+    category: "DJ Event",
+    slug: "hype-hijacking-tryst-2023",
+    image:
+      "https://res.cloudinary.com/dta1dl0pj/image/upload/q_auto/f_auto/v1778519894/hype_hijacking_tryst_Tej_DJ_event_2023_ef7w95.png",
+  },
+
+  {
+    title: "Yzy Slides AD",
+    category: "Product Marketing",
+    slug: "yzy-slides-mainstreet-marketplace",
+    image:
+      "https://res.cloudinary.com/dta1dl0pj/image/upload/q_auto/f_auto/v1778519894/Yzy_Slides_AD_for_Mainstreet_Marketplace_wgilel.jpg",
+  },
+
+  {
+    title: "Acoustic Night",
+    category: "Live Music",
+    slug: "acoustic-night-amann-soni-2024",
+    image:
+      "https://res.cloudinary.com/dta1dl0pj/image/upload/q_auto/f_auto/v1778519894/acoustic_night_by_amann_soni_Saqi_2024_ouah9y.png",
+  },
+
+  {
+    title: "Taj Lands End Bar Night",
+    category: "Tech House Event",
+    slug: "taj-lands-end-tech-house-2025",
+    image:
+      "https://res.cloudinary.com/dta1dl0pj/image/upload/q_auto/f_auto/v1778519894/Taj_Lands_End_bar_Night_Tech_House_2025_p0mnkd.jpg",
+  },
+
+  {
+    title: "Bollywood Soirée",
+    category: "Event Branding",
+    slug: "bollywood-soiree-july",
+    image:
+      "https://res.cloudinary.com/dta1dl0pj/image/upload/q_auto/f_auto/v1778519893/Copy_of_bollywood_soiree_20_th_july_v1.1_uezspp.png",
+  },
+
+  {
+    title: "Akhil The Singer Concert",
+    category: "Concert Poster",
+    slug: "akhil-singer-concert-2024",
+    image:
+      "https://res.cloudinary.com/dta1dl0pj/image/upload/q_auto/f_auto/v1778519891/Akhil_The_Singer_Concert_2024_dzjtbd.jpg",
+  },
+
+  {
+    title: "Chic Therapy Marketing",
+    category: "AD Marketing",
+    slug: "chic-therapy-marketing-2025",
+    image:
+      "https://res.cloudinary.com/dta1dl0pj/image/upload/q_auto/f_auto/v1778519891/Chic_Therapy_AD_Marketing_2025_lob323.jpg",
+  },
+
+  {
+    title: "House of Horrors",
+    category: "BMS Banner",
+    slug: "house-of-horrors-2024",
+    image:
+      "https://res.cloudinary.com/dta1dl0pj/image/upload/q_auto/f_auto/v1778519893/House_of_Horrors_book_my_show_banner_2024_kmwwjw.png",
+  },
+
+  {
+    title: "Chic Therapy Decor",
+    category: "Performance Marketing",
+    slug: "chic-therapy-decor-performance",
+    image:
+      "https://res.cloudinary.com/dta1dl0pj/image/upload/q_auto/f_auto/v1778519891/Chic_Thearpy_Decor_Ad_for_Performance_Marketing_bjcktk.jpg",
+  },
 ];
 
 export default projects;
